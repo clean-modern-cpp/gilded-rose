7da06db384ba79a9da8cf7b7b605678893315760
codingpotato <codingpotato@gmail.com>
2021-01-30 10:42:50 +0800

    Basic requirement

--- src/GildedRose.h
+++ src/GildedRose.h
@@ -8,14 +8,36 @@ struct GildedRose {
 
   void updateQuality() {
     for (auto& item : items) {
-      if (item.sellIn > 0) {
-        --item.sellIn;
-        if (item.quality > 0) {
-          --item.quality;
-        }
-      } else {
-        if (item.quality >= 2) {
-          item.quality -= 2;
+      if (item.name != "Sulfuras") {
+        if (item.sellIn > 0) {
+          --item.sellIn;
+          if (item.name == "Aged Brie") {
+            if (item.quality < 50) {
+              ++item.quality;
+            }
+          } else if (item.name == "Backstage pass") {
+            if (item.sellIn >= 10) {
+              if (item.quality < 50) {
+                ++item.quality;
+              }
+            } else if (item.sellIn >= 5) {
+              if (item.quality < 49) {
+                item.quality += 2;
+              }
+            } else if (item.sellIn > 0) {
+              if (item.quality < 48) {
+                item.quality += 3;
+              }
+            } else {
+              item.quality = 0;
+            }
+          } else if (item.quality > 0) {
+            --item.quality;
+          }
+        } else {
+          if (item.quality >= 2) {
+            item.quality -= 2;
+          }
         }
       }
     }
--- src/GildedRoseTests.cpp
+++ src/GildedRoseTests.cpp
@@ -2,7 +2,7 @@
 
 #include "GildedRose.h"
 
-TEST_CASE("Quality drop 1 when sellIn greater than 0") {
+TEST_CASE("Quality degrades 1 when sellIn greater than 0") {
   GildedRose gildedRose{{
       {"Name", 1, 1},
   }};
@@ -12,7 +12,7 @@ TEST_CASE("Quality drop 1 when sellIn greater than 0") {
                                });
 }
 
-TEST_CASE("Quality drop 2 when sellIn equal to 0") {
+TEST_CASE("Quality degrades 2 when sellIn equal to 0") {
   GildedRose gildedRose{{
       {"Name", 0, 2},
   }};
@@ -29,5 +29,78 @@ TEST_CASE("Quality always greater or equal to 0") {
   gildedRose.updateQuality();
   REQUIRE_EQ(gildedRose.items, Items{
                                    {"Name", 0, 0},
+                               });
+}
+
+TEST_CASE("Aged Brie quality increases by 1") {
+  GildedRose gildedRose{{
+      {"Aged Brie", 1, 1},
+  }};
+  gildedRose.updateQuality();
+  REQUIRE_EQ(gildedRose.items, Items{
+                                   {"Aged Brie", 0, 2},
+                               });
+}
+
+TEST_CASE("Quality always less or equal to 50") {
+  GildedRose gildedRose{{
+      {"Aged Brie", 1, 50},
+  }};
+  gildedRose.updateQuality();
+  REQUIRE_EQ(gildedRose.items, Items{
+                                   {"Aged Brie", 0, 50},
+                               });
+}
+
+TEST_CASE("SellIn and quality never change for Sulfuras") {
+  GildedRose gildedRose{{
+      {"Sulfuras", 1, 1},
+  }};
+  gildedRose.updateQuality();
+  REQUIRE_EQ(gildedRose.items, Items{
+                                   {"Sulfuras", 1, 1},
+                               });
+}
+
+TEST_CASE(
+    "Backstage pass quality increase by 1 when there are more than 10 days") {
+  GildedRose gildedRose{{
+      {"Backstage pass", 11, 1},
+  }};
+  gildedRose.updateQuality();
+  REQUIRE_EQ(gildedRose.items, Items{
+                                   {"Backstage pass", 10, 2},
+                               });
+}
+
+TEST_CASE(
+    "Backstage pass quality increase by 2 when there are 10 days or less") {
+  GildedRose gildedRose{{
+      {"Backstage pass", 10, 1},
+  }};
+  gildedRose.updateQuality();
+  REQUIRE_EQ(gildedRose.items, Items{
+                                   {"Backstage pass", 9, 3},
+                               });
+}
+
+TEST_CASE(
+    "Backstage pass quality increase by 3 when there are 5 days or less") {
+  GildedRose gildedRose{{
+      {"Backstage pass", 5, 1},
+  }};
+  gildedRose.updateQuality();
+  REQUIRE_EQ(gildedRose.items, Items{
+                                   {"Backstage pass", 4, 4},
+                               });
+}
+
+TEST_CASE("Backstage pass quality drops to 0 after the concert") {
+  GildedRose gildedRose{{
+      {"Backstage pass", 1, 10},
+  }};
+  gildedRose.updateQuality();
+  REQUIRE_EQ(gildedRose.items, Items{
+                                   {"Backstage pass", 0, 0},
                                });
 }
